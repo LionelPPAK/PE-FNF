1c9101c1351a47981e74f4515048af39e157aa89
LionelPPAK <124942396+LionelPPAK@users.noreply.github.com>
2023-03-26 01:04:14 -0500

    CCΠ

--- source/MCST.hx
+++ source/MCST.hx
@@ -69,6 +69,8 @@ class MCST extends FlxSubState
 			StageData.loadDirectory(_song);
 			LoadingState.loadAndSwitchState(new PlayState());
 			}
+	}
+}
 		exitButton.setGraphicSize(Std.int(exitButton.width) * 3);
 		exitButton.label.setFormat(Assets.getFont('assets/mobile/menu/vcr.ttf').fontName, 21, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE,
 			FlxColor.BLACK, true);
