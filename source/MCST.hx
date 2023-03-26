625b81f5de673094c56d6346f8aa506a88305ede
LionelPPAK <124942396+LionelPPAK@users.noreply.github.com>
2023-03-26 14:58:46 -0500

    A

--- source/MCST.hx
+++ source/MCST.hx
@@ -68,7 +68,6 @@ class MCST extends FlxSubState
 			if(vocals != null) vocals.stop();
 			StageData.loadDirectory(_song);
 			LoadingState.loadAndSwitchState(new PlayState());
-			}
 		});
 		exitButton.setGraphicSize(Std.int(exitButton.width) * 3);
 		exitButton.label.setFormat(Assets.getFont('assets/mobile/menu/vcr.ttf').fontName, 21, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE,
